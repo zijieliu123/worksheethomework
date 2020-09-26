fe98cdd419f7ab3554e82c272e12f0dc5f91d74f
jimmy <jimmy@localhost>
2020-09-26 14:01:21 +0800

    pushinglevel

---
+++ homework/src/module-info.java
@@ -0,0 +1,15 @@
+public class homework {
+	
+	public void inToChar(int x) {
+		System.out.println(charx);	
+	}
+	public void lastDigit(int x) {
+		System.out.println(x % 10);
+	}
+	public static void main(String[] args) {
+		
+		inToChar(5)
+		lastDigit(10)
+		
+	}
+}
